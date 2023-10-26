64b2ff5bcf39c52fef4855dc4e7ccab70e94563b
jonatas ribeiro <jonatas@prottaribeiro.com>
2023-10-26 21:09:46 +0100

    first ruby code

---
+++ PRACTICAL/RUBY LEARNING/hello.rb
@@ -0,0 +1,11 @@
+puts "HELLO WORLD"
+
+def test_function (first_argument):
+    puts "This is my function"
+    puts %d "The argument i am receiving is:" first_argument
+
+    return true
+
+end
+
+test_function "Jonatas"
